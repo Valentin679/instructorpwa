4e2c1136a3c0f921e57b2f55dda9ffdc45ee1218
Valentin679 <67257342+Valentin679@users.noreply.github.com>
2024-12-17 19:48:29 +0300

    manifest

--- app/layout.js
+++ app/layout.js
@@ -21,6 +21,7 @@ export default function RootLayout({children}) {
     return (
         <html lang="en">
         <head>
+            <link rel="manifest" href="manifest.webmanifest"/>
             <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
                   integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH"
                   crossOrigin="anonymous"/>
