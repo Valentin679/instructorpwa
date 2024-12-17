42984c366698930b02b6224f106d8faeebd5c89b
Valentin679 <67257342+Valentin679@users.noreply.github.com>
2024-12-17 20:33:26 +0300

    studentsPage

--- app/components/Nav/NavItem.js
+++ app/components/Nav/NavItem.js
@@ -1,13 +1,14 @@
 import {BsFillPeopleFill} from "react-icons/bs";
 import {FaTableList} from "react-icons/fa6";
+import Link from "next/link";
 
 export default function NavItem({icon, label}) {
     const isActive = false
     return (
-        <a className={`navbar-brand p-3 d-flex flex-column align-items-center ${isActive ? "text-danger" : "text-primary"}`} href="#">
+        <Link className={`navbar-brand p-3 d-flex flex-column align-items-center ${isActive ? "text-danger" : "text-primary"}`} href="/students">
             {icon}
             <p className="text-sm m-0">{label}</p>
-        </a>
+        </Link>
     );
 }
 
---
+++ app/students/page.js
@@ -0,0 +1,7 @@
+import Image from "next/image";
+
+export default function Students() {
+  return (
+      <div style={{minHeight: '100%', height: '1000px'}}>Ученики</div>
+  );
+}
