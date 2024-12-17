77c9bfd80c91a7157e27b4c1288a17ec77242bdc
Valentin679 <67257342+Valentin679@users.noreply.github.com>
2024-12-17 22:29:14 +0300

    headers

--- app/api/fetchStudents.js
+++ app/api/fetchStudents.js
@@ -6,7 +6,13 @@ const instance = {
 export async function getStudents() {
     const response = await fetch(`${instance.url}api/students/category/b`, {
         method: "GET",
-        headers: { "Accept": "application/json" }
+        headers: [
+            { key: "Access-Control-Allow-Credentials", value: "true" },
+            { key: "Access-Control-Allow-Origin", value: "*" },
+            { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
+            { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+        ]
+        // headers: { "Accept": "application/json" }
     });
     if (response.ok === true) {
         return await response.json()
