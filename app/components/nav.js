a51d7df6be2d4b126bf44f3579029aeff12a781a
Valentin679 <67257342+Valentin679@users.noreply.github.com>
2024-12-17 19:08:01 +0300

    stickyBot

---
+++ app/components/nav.js
@@ -0,0 +1,12 @@
+export default function Nav() {
+    return (
+        <nav className="navbar sticky-bottom bg-body-tertiary">
+            <div className="container-fluid">
+                <a className="navbar-brand" href="#">Sticky bottom</a>
+            </div>
+        </nav>
+    );
+}
+
+
+
