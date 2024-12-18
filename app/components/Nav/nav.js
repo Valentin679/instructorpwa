a4af31d64c89c667fdb48e5aea363415e85676c1
Rat <mezeninvalentin@yandex.ru>
2024-12-18 23:30:07 +0300

    saveGrades

--- app/components/Nav/nav.js
+++ app/components/Nav/nav.js
@@ -1,11 +1,13 @@
 import {BsFillPeopleFill} from "react-icons/bs";
 import {FaTableList} from "react-icons/fa6";
 import NavItem from "@/app/components/Nav/NavItem";
+import {ImProfile} from "react-icons/im";
 
 export default function Nav() {
     return (
         <nav className="navbar sticky-bottom bg-body-tertiary p-2">
             <div className="container-fluid d-flex justify-content-between">
+                <NavItem label={'Профиль'} icon={<ImProfile />} link={'/'}/>
                 <NavItem label={'Ученики'} icon={<BsFillPeopleFill />} link={'/students'}/>
                 <NavItem label={'Расписание'} icon={<FaTableList />} link={'/schedule'}/>
             </div>
