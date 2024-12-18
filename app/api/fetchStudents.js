7d562c4620b5057cc01606744b6bb88e4b37593c
Rat <mezeninvalentin@yandex.ru>
2024-12-19 00:59:08 +0300

    gradesSave1

--- app/api/fetchStudents.js
+++ app/api/fetchStudents.js
@@ -36,12 +36,12 @@ export const addStudent = async (data) => {
     }
 }
 
-export const editStudentGrades = async (id, slug, level, name, gradeIndex) => {
+export const editStudentGrades = async (id, slug, level, gradeIndex) => {
     await fetch(`${instance.url}api/students/category/b/edit`, {
         method: 'PUT',
         //
         body: JSON.stringify({
-            id, slug, level, name, gradeIndex
+            id, slug, level, gradeIndex
         }),
         headers: {
             'Content-type': 'application/json; charset=utf-8'
