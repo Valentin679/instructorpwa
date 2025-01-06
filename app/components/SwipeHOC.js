c70fa6a62485a7e7cb53115150ee074e8b25025d
Valentin679 <67257342+Valentin679@users.noreply.github.com>
2025-01-06 16:17:56 +0300

    swiper

--- app/components/SwipeHOC.js
+++ app/components/SwipeHOC.js
@@ -4,8 +4,6 @@ import SwipeContext from "@/app/context/SwipeContext";
 
 export default function SwipeHOC({children, refLink, refLeftMenu, refRightMenu, refContainer}) {
     const [openRight, setOpenRight] = useState(false)
-    const [openLeft, setOpenLeft] = useState(false)
-    const [touchDifference, setTouchDifference] = useState(0)
 
     let defWidth = window.innerWidth
     let fiveHalf = defWidth * 0.15
@@ -13,131 +11,62 @@ export default function SwipeHOC({children, refLink, refLeftMenu, refRightMenu, 
 
 
     function checkTouchStart() {
-        if (openLeft === false && openRight === false) {
-            let touchStartX = 0
-            let touchMove = 0
-            refLink.current.addEventListener('touchstart', (e) => {
-                touchStartX = e.changedTouches[0].pageX
-                console.log(touchStartX)
-            })
-            refLink.current.addEventListener('touchmove', (e) => {
-                if (touchStartX > defWidth - fiveHalf && !openRight && !openLeft) {
-
-                    touchMove = e.changedTouches[0].pageX
-                    let diff = touchStartX - touchMove
-                    refLink.current.style.right = diff + 'px'
-                    refRightMenu.current.style.width = diff + 'px'
-                    refRightMenu.current.style.right = diff * 2 + 'px'
-                    refContainer.current.style.width = defWidth + diff * 2 + 'px'
-                }
-
-                if (touchStartX < fiveHalf && !openRight && !openLeft) {
-                    console.log(openRight)
-                    touchMove = e.changedTouches[0].pageX
-                    let diff = touchStartX - touchMove
-                    // refLink.current.style.right = diff + 'px'
-                    refLeftMenu.current.style.width = -diff + 'px'
-                    refLeftMenu.current.style.right = diff + 'px'
-                    refContainer.current.style.width = defWidth - diff + 'px'
-                }
-            })
-            refLink.current.addEventListener('touchend', (e) => {
-                let rightMenuWidth = Number(refRightMenu.current.style.width.slice(0, -2))
-                let leftMenuWidth = Number(refLeftMenu.current.style.width.slice(0, -2))
-                // console.log(leftMenuWidth)
-                if (rightMenuWidth === 0 || leftMenuWidth === 0) {
-                    // console.log('меня не двигали')
-                }
-                if (50 > rightMenuWidth > 0) {
-                    refRightMenu.current.style.width = 0
-                    refLink.current.style.right = 0
-                    setOpenRight(false)
-                }
-                if (50 > leftMenuWidth > 0) {
-                    refLeftMenu.current.style.width = 0
-                    refLink.current.style.right = 0
-                    setOpenLeft(false)
-                }
-                if (rightMenuWidth > 50) {
-                    let width = WIDTH_MENU * refRightMenu.current.childElementCount
-                    refRightMenu.current.style.width = width + 'px'
-                    refLink.current.style.right = width + 'px'
-                    setOpenRight(true)
-                }
-                if (leftMenuWidth > 50) {
-                    let width = WIDTH_MENU * refLeftMenu.current.childElementCount
-                    refLeftMenu.current.style.width = width + 'px'
-                    refLink.current.style.right = width + 'px'
-                    setOpenLeft(true)
-                }
-            })
-        }
+        let touchStartX = 0
+        let touchMove = 0
+        refLink.current.addEventListener('touchstart', (e) => {
+            touchStartX = e.changedTouches[0].pageX
+        })
+        refLink.current.addEventListener('touchmove', (e) => {
+            let rightMenuWidth = Number(refRightMenu.current.style.width.slice(0, -2))
+            if (touchStartX > defWidth - fiveHalf && openRight === false) {
+                touchMove = e.changedTouches[0].pageX
+                let diff = touchStartX - touchMove
+                refLink.current.style.right = diff + 'px'
+                refRightMenu.current.style.width = diff + 'px'
+                refRightMenu.current.style.right = diff * 2 + 'px'
+                refContainer.current.style.width = defWidth + diff * 2 + 'px'
+            } else if (openRight !== false && rightMenuWidth !== 0 && touchStartX < defWidth - rightMenuWidth) {
+                touchMove = e.changedTouches[0].pageX
+                let diff = touchStartX - touchMove
+                refContainer.current.style.width = defWidth - diff + rightMenuWidth + 'px'
+                refLink.current.style.right = rightMenuWidth + diff + 'px'
+                refRightMenu.current.style.right = rightMenuWidth + diff + 'px'
+                refRightMenu.current.style.width = '0 px'
+            }
+        })
+        refLink.current.addEventListener('touchend', (e) => {
+            let rightMenuWidth = Number(refRightMenu.current.style.width.slice(0, -2))
+            let leftMenuWidth = Number(refLeftMenu.current.style.width.slice(0, -2))
+            if (rightMenuWidth === 0 || leftMenuWidth === 0) {
+                // console.log('меня не двигали')
+            } else if (50 > rightMenuWidth > 0) {
+                refRightMenu.current.style.width = 0
+                refLink.current.style.right = 0
+                setOpenRight(false)
+            }
+            if (rightMenuWidth > 50 && touchStartX < defWidth - rightMenuWidth) {
+                refContainer.current.style.width = defWidth + 'px'
+                refLink.current.style.right = 0 + 'px'
+                refRightMenu.current.style.right = 0 + 'px'
+                refRightMenu.current.style.width = 0 + 'px'
+                setOpenRight(false)
+            } else if (rightMenuWidth > 50) {
+                let width = WIDTH_MENU * refRightMenu.current.childElementCount
+                refContainer.current.style.width = defWidth +  width  +'px'
+                refLink.current.style.right = 100 + 'px'
+                refRightMenu.current.style.width = 100 + 'px'
+                refRightMenu.current.style.right = 100 + 'px'
+                setOpenRight(true)
+            }
+        })
     }
 
-    function checkClose() {
-        if (openLeft || openRight) {
-            let touchStartX = 0
-            let touchMove = 0
-            refLink.current.addEventListener('touchstart', (e) => {
-                touchStartX = e.changedTouches[0].pageX
-                // console.log(touchStartX)
-            })
-
-
-            // if (openRight) {
-            //     console.log(openRight)
-            //     let diff = touchMove - touchStartX
-            //     if (diff > 100) {
-            //         console.log('>100')
-            //     }
-            // }
-        }
-    }
 
     useEffect(() => {
-            checkTouchStart()
-    }, [openLeft, openRight]);
+        checkTouchStart(openRight)
+    }, [openRight]);
 
-    useEffect(() => {
-        if (openLeft || openRight) {
-            checkClose()
-            console.log(openRight)
-        }
-    }, [openRight, openLeft]);
-    // useEffect(() => {
-    //     let touchStartX = 0
-    //     let touchMove = 0
-    //     defWidth = ref.current.clientWidth
-    //     // console.log(defWidth)
-    //     ref.current.addEventListener('touchstart', (e) => {
-    //         touchStartX = e.changedTouches[0].pageX
-    //         // console.log(touchStartX)
-    //     })
-    //
-    //     ref.current.addEventListener('touchmove', (e) => {
-    //         touchMove = e.changedTouches[0].pageX
-    //         if (touchStartX - touchMove < 50) {
-    //             setTouchDifference(touchStartX - touchMove)
-    //             defWidth = defWidth + (touchStartX - touchMove)
-    //         }
-    //         if (touchStartX - touchMove > 50) {
-    //             setTouchDifference(50)
-    //             defWidth = defWidth + 50
-    //             setOpenRight(true)
-    //
-    //         }
-    //         if (touchStartX - touchMove < -10) {
-    //             setTouchDifference(0)
-    //         }
-    //     })
-    //
-    // }, []);
 
-    // useEffect(() => {
-    //     // console.log(openRight)
-    // }, [openRight]);
-    // useEffect(() => {
-    // }, [touchDifference]);
     return (
         <SwipeContext.Provider value={0}>
             {children}
